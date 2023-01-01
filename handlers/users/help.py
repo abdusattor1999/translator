7102efb607cb2f72dc8d6d5be2be201fa034ce5f
abdusattor1999 <100144478+abdusattor1999@users.noreply.github.com>
2023-01-01 12:45:20 +0500

    Update help.py

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -3,9 +3,10 @@ from aiogram.dispatcher.filters.builtin import CommandHelp
 
 from loader import dp
 
-
+@dp.message_handler(commands='help', state=ChooseOption)
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
     text = ("Buyruqlar: ",
-            "/start - 🔄 Botni qayta ishga tushirish")
+            "/start - 🔄 Botni qayta ishga tushirish"
+           )
     await message.answer("\n".join(text))
