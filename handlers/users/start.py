aa111033ffd92362f51484632bb7ad9b90d49334
abdusattor1999 <100144478+abdusattor1999@users.noreply.github.com>
2023-01-01 13:02:41 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,4 +1,5 @@
 from aiogram import types
+from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.builtin import CommandStart
 from keyboards.default.main_keyboards import start
 from loader import dp
@@ -6,5 +7,7 @@ from states.user_control_state import ChooseOption
 
 @dp.message_handler(commands='start', state=ChooseOption)
 @dp.message_handler(CommandStart())
-async def bot_start(message: types.Message):
+async def bot_start(message: types.Message, state=FSMContext):
+    if state:
+        state.finish()
     await message.answer(f"Assalomu aleykum, <b>{message.from_user.full_name} !</b>\nBotga xush kelibsiz \n👇 Pastdagi tugmalar orqali kerakli opsiyani tanlang",reply_markup=start)
