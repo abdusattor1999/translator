33f84c508defe89b7bf842e50e9817dd62359ddd
abdusattor1999 <100144478+abdusattor1999@users.noreply.github.com>
2023-01-01 12:43:36 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -3,7 +3,7 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 from keyboards.default.main_keyboards import start
 from loader import dp
 
-
+@dp.message_handler(commands='start', state=ChooseOption)
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Assalomu aleykum, <b>{message.from_user.full_name} !</b>\nBotga xush kelibsiz \n👇 Pastdagi tugmalar orqali kerakli opsiyani tanlang",reply_markup=start)
